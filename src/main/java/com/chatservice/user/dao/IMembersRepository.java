91383b22f67f19350703bd3d7d9805d4862a0b41
bumjinDev <bumjin.dev@gmail.com>
2025-05-19 12:17:32 +0900

    feat: 유저 생성/조회/예외처리 기능 구현

---
+++ src/main/java/com/chatservice/user/dao/IMembersRepository.java
@@ -0,0 +1,18 @@
+package com.chatservice.user.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.chatservice.auth.exception.dto.AuthenticationEntity;
+import com.chatservice.user.model.MembersEntity;
+
+
+
+public interface IMembersRepository {
+	
+	public void addMember(MembersEntity membersEntity, AuthenticationEntity userEntity);			// confimMember() 실행 시 이상 없으면 회원 가입 진행.
+	public Optional<MembersEntity>  getMember(String userId);						// modify.jsp 페이지 제공 위한 MembersVO
+	List<MembersEntity> getMembers(List<String> userIds);
+	public void editMember(MembersEntity memberVO, AuthenticationEntity userEntity);					// 회원정보 수정
+	Optional<MembersEntity> isNEditickNameAllowed(MembersEntity membersEntity, AuthenticationEntity userEntity);
+}
