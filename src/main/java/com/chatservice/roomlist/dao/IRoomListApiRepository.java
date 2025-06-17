31c2592639a5e0815820299bb5fb03a0301fb548
bumjinDev <bumjin.dev@gmail.com>
2025-06-17 11:13:34 +0900

    roomlist: 백업 복원 및 변경사항 커밋

--- src/main/java/com/chatservice/roomlist/dao/IRoomListApiRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chatservice.roomlist.dao;
-
-import java.util.List;
-
-public interface IRoomListApiRepository {
-
-	List<RoomListEntity> searchRoomList();
-}
--- src/main/java/com/chatservice/roomlist/service/RoomListApiService.java
+++ src/main/java/com/chatservice/roomlist/service/RoomListApiService.java
@@ -1,30 +1,34 @@
 package com.chatservice.roomlist.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
-import com.chatservice.roomlist.dao.IRoomListApiRepository;
+import com.chatservice.joinroom.dao.ChatRoom;
+import com.chatservice.websocketcore.model.ChatSessionRegistry;
 import com.chatservice.roomlist.model.RoomDTO;
-
 import com.chatservice.roomlist.model.RoomListConverter;
 
 @Service
-public class RoomListApiService implements IRoomListApiService{
+public class RoomListApiService implements IRoomListApiService {
 
-	IRoomListApiRepository roomListApiRepository;
-	RoomListConverter roomConverter;
-	
+	private final RoomListConverter roomConverter;
+	private final ChatSessionRegistry chatSessionRegistry;
+
 	public RoomListApiService(
-					IRoomListApiRepository roomListApiRepository,
-					RoomListConverter roomConverter) {
-		
-		this.roomListApiRepository = roomListApiRepository;
+			RoomListConverter roomConverter,
+			ChatSessionRegistry chatSessionRegistry) {
 		this.roomConverter = roomConverter;
+		this.chatSessionRegistry = chatSessionRegistry;
 	}
-	
+
 	@Override
 	public List<RoomDTO> getRoomList() {
-		return roomConverter.toDtoList(roomListApiRepository.searchRoomList());
+		// 1. ChatRoom 목록 조회 (실제 데이터 소스는 ChatSessionRegistry, DB, 캐시 등)
+		List<ChatRoom> chatRoomList = new ArrayList<>(chatSessionRegistry.getRooms().values());
+		// 2. 변환기로 DTO 리스트 변환 후 반환
+		return roomConverter.toDtoList(chatRoomList);
 	}
 }
